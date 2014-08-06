d36375165fc978d75338c57c463ce36399904600
Niyog Ray <niyog@leftshift.io>
2014-08-06 12:29:27 +0530

    + CollectionViewCell

---
+++ CustomCollectionViewLayout/LSAlbumPhotoCell.m
@@ -0,0 +1,32 @@
+//
+//  LSAlbumPhotoCell.m
+//  CustomCollectionViewLayout
+//
+//  Created by Niyog Ray on 06/08/14.
+//  Copyright (c) 2014 LeftShift. All rights reserved.
+//
+
+#import "LSAlbumPhotoCell.h"
+
+@implementation LSAlbumPhotoCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        self.backgroundColor = [UIColor colorWithWhite:.85 alpha:1.];
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
